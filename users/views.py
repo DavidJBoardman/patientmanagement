cd20aec13c0c99c0dd297495e83068139dd5a189
David Boardman <did.boardman@gmail.com>
2018-10-23 11:58:20 +0100

    Added profile page which can only be accessible if you're logged in. Forced login and start for user restricted views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Handles user registration
@@ -11,8 +12,12 @@ def register(request):
             # Automatically hashes password and saves user
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('mysite-home')
+            messages.success(request, f'Your account has now been created, Welcome {username}!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
