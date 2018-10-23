610b6609928502e8166be9f6d1b8fd25820416bb
David Boardman <did.boardman@gmail.com>
2018-10-23 16:37:19 +0100

    Login is required to view the app.

--- mysite/views.py
+++ mysite/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .models import PersonalDetails
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 # Example of creating a view (in the future this should be a template)
+@login_required()
 def home(request):
     return render(request, 'mysite/home.html', {'title': 'Home'})
 
 # Patient details page
+@login_required()
 def patient(request):
     context = {
         'patients': PersonalDetails.objects.all(),
