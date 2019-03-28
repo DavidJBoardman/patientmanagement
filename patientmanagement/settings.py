8b454db96f4268edae56bb3a9c53e88de9f1af63
didlid <did.boardman@gmail.com>
2019-03-28 23:57:55 +0000

    - Changes for heroku hosting

--- patientmanagement/settings.py
+++ patientmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ LOGIN_REDIRECT_URL = 'mysite-home'
 LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
