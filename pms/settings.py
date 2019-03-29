f37f7bd093a096f817c9b1ab7cf9dad2073644ce
didlid <did.boardman@gmail.com>
2019-03-29 00:28:46 +0000

    Heroku

--- pms/settings.py
+++ pms/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'patientmanagement.urls'
+ROOT_URLCONF = 'pms.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'patientmanagement.wsgi.application'
+WSGI_APPLICATION = 'pms.wsgi.application'
 
 
 # Database
