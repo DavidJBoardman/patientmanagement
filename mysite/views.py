2016694e9ccacb1ccd428e06ee1c2b39bc762af0
didlid <did.boardman@gmail.com>
2019-03-29 19:55:30 +0000

    Added edit personal details to patient view

--- mysite/views.py
+++ mysite/views.py
@@ -48,7 +48,6 @@ def home(request):
     patients = paginator.get_page(page)
     context = {
         'patients': patients,
-        'patient': patient,
         'personal_list': personal_patients,
         'title': 'Home',
         'form': form
